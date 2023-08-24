3e1bbb700b05fd866d2e88582ffd402c271f5e3a
Parigiffy <141135868+Parigiffy@users.noreply.github.com>
2023-08-24 17:30:32 +0100

    Update ShopCategory.js

--- src/Components/ShopCategory.js
+++ src/Components/ShopCategory.js
@@ -30,9 +30,9 @@ const ShopCategory = () => {
               {category.name}
             </p>
           ))
-        ) : (
+        ) : 
           <Spinner/>
-        )}
+        }
       </div>
     </Col>
   );
