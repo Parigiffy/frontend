53042827781fee49813ccbc032dc08b892cc8677
Parigiffy <141135868+Parigiffy@users.noreply.github.com>
2023-08-24 16:48:19 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -59,11 +59,11 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path='/contact' element={<ContactUs />} />
+           <Route path='/contact' element={<ContactUs />} />
         // <Route path='/' element={user ? <Navigate to='/home' /> : <Signin />} />
           <Route path='/' element={ <Home />} />
 
-        <Route path='/home' element={<Home />} />
+          <Route path='/home' element={<Home />} />
         // <Route path='/signup' element={user ? <Navigate to='/home' /> : <Signup />} />
         // {user && (
         //   <>
@@ -79,28 +79,28 @@ function App() {
         //   </>
         // )}
 
-           // <Route path='/wishlists' element={<Lists />} />
-           //  <Route path='/cart' element={<CartSection />} />
-           //  <Route path='/payment' element={<Payment />} />
-           //  <Route path='/orders' element={<Orders />} />
-           //  <Route path='/account' element={<Profile />} />
-           //  <Route path='/product/:id' element={<ProductPage />} />
-           //  <Route path='/category/:id' element={<Category />} />
-           //  <Route path='*' element={<Error />} />
-           //  <Route path='/allproducts' element={<AllProducts />} />
+           <Route path='/wishlists' element={<Lists />} />
+            <Route path='/cart' element={<CartSection />} />
+            <Route path='/payment' element={<Payment />} />
+            <Route path='/orders' element={<Orders />} />
+            <Route path='/account' element={<Profile />} />
+            <Route path='/product/:id' element={<ProductPage />} />
+            <Route path='/category/:id' element={<Category />} />
+            <Route path='*' element={<Error />} />
+            <Route path='/allproducts' element={<AllProducts />} />
          
-        <React.Fragment>
-          <Route path='/ero-admin/login' element={<AdminLogin />} />
-          <Route path='/ero-admin' element={<AdminPanel />} />
-          <Route path='/admin/products' element={<ProductsView />} />
-          <Route path='/admin/addProducts' element={<AddProducts />} />
+        // <React.Fragment>
+        //   <Route path='/ero-admin/login' element={<AdminLogin />} />
+        //   <Route path='/ero-admin' element={<AdminPanel />} />
+        //   <Route path='/admin/products' element={<ProductsView />} />
+        //   <Route path='/admin/addProducts' element={<AddProducts />} />
 
-          <Route path='/admin/payments' element={<Payments />} />
-          <Route path='/admin/editProduct/:id' element={<EditProducts />} />
-          <Route path='/admin/orders' element={<OrderDetails />} />
-          <Route path='/admin/users' element={<Users />} />
-          <Route path='/admin/departments' element={<Departments />} />
-        </React.Fragment>
+        //   <Route path='/admin/payments' element={<Payments />} />
+        //   <Route path='/admin/editProduct/:id' element={<EditProducts />} />
+        //   <Route path='/admin/orders' element={<OrderDetails />} />
+        //   <Route path='/admin/users' element={<Users />} />
+        //   <Route path='/admin/departments' element={<Departments />} />
+        // </React.Fragment>
       </Routes>
     </BrowserRouter>
   );
