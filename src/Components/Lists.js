0a3e1ba49ee1fb7b167882f446fc0691002bbc73
Parigiffy <141135868+Parigiffy@users.noreply.github.com>
2023-08-28 13:58:19 +0100

    Update Lists.js

--- src/Components/Lists.js
+++ src/Components/Lists.js
@@ -103,8 +103,6 @@ function Lists() {
                       </div>
                       <div className='card1-price'>
                         <p className='discount'>${items.price}</p>
-                        <p className='mrp'>${Math.round(items.price * 1.66)}</p>
-                        <p className='price-off'>(60% OFF)</p>
                       </div>
                     </div>
                   </div>
