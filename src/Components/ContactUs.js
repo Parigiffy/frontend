184f8b9a514b2689c333a2a5ecb5e4b2233ec54f
Parigiffy <141135868+Parigiffy@users.noreply.github.com>
2023-08-30 18:35:52 +0100

    Update ContactUs.js

--- src/Components/ContactUs.js
+++ src/Components/ContactUs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import TopNav from "./TopNav";
 import Navbar from "./Navbar";
 import { toast, ToastContainer } from "react-toastify";
